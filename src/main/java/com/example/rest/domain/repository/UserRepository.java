155932f633d5b1ef03b04a30096137a9f2d345dd
uetak330 <uetak330@gmail.com>
2024-11-10 23:00:23 +0900

    UserRepositoryの作成

--- src/main/java/com/example/rest/domain/repository/UserRepository.java
+++ src/main/java/com/example/rest/domain/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.example.rest.domain.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.example.rest.domain.mapper.UserMapper;
+import com.example.rest.domain.model.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,4 +15,23 @@ public class UserRepository {
 	
 	private final UserMapper userMapper;
 	
+	public List<User> findAll() {
+		return userMapper.findAll();
+	}
+	
+	public User findByIdUser(Long id) {
+		return userMapper.findById(id);
+	}
+	
+	public int create(User user) {
+		return userMapper.insert(user);
+	}
+	
+	public int update(User user) {
+		return userMapper.update(user);
+	}
+	
+	public int deleteById(Long id) {
+		return userMapper.deleteById(id);
+	}
 }
