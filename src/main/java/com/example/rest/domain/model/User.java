1e9218c8be352fb2b3e891b303b6d0c751c57e3f
uetak330 <uetak330@gmail.com>
2024-11-10 15:53:23 +0900

    エンティティの作成

---
+++ src/main/java/com/example/rest/domain/model/User.java
@@ -0,0 +1,20 @@
+package com.example.rest.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	
+	private Long id;
+	
+	private String name;
+	
+	private Integer age;
+	
+	private String address;
+
+}
