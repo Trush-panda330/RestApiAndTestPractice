e3d89db579ebb924845f43417b6bae62f49c0d8b
uetak330 <uetak330@gmail.com>
2024-11-10 22:36:30 +0900

    バリデーションの追記

--- src/main/java/com/example/rest/domain/model/User.java
+++ src/main/java/com/example/rest/domain/model/User.java
@@ -1,5 +1,8 @@
 package com.example.rest.domain.model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,12 +12,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class User {
 	
+	@NotNull(message = "IDは必須です。")
 	private Long id;
 	
+	@NotBlank(message="名前は必須です。")
 	private String name;
 	
+	@Min(value = 0, message="年齢は0歳以上で設定してください。")
 	private Integer age;
 	
+	@NotBlank(message = "住所は必須です。")
 	private String address;
 
 }
