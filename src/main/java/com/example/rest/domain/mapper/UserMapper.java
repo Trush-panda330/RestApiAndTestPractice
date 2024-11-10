262e3ed52a22d19aad520aeb8521326cc3a48c2f
uetak330 <uetak330@gmail.com>
2024-11-10 15:57:46 +0900

    UserMapperインターフェースを定義

---
+++ src/main/java/com/example/rest/domain/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.example.rest.domain.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.example.rest.domain.model.User;
+
+@Mapper
+public interface UserMapper {
+	
+	List<User> findAll();
+	
+	User findById(@Param("id") Long id);
+	
+	int insert(@Param("user") User user);
+	
+	int update(@Param("user") User user);
+	
+	int delete(@Param("id") Long id);
+
+}
