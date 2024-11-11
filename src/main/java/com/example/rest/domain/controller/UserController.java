dfb641072682eb9d7a397573e0edba84817ae549
uetak330 <uetak330@gmail.com>
2024-11-11 10:58:55 +0900

    POSTメソッドとDELETEメソッドの追加

--- src/main/java/com/example/rest/domain/controller/UserController.java
+++ src/main/java/com/example/rest/domain/controller/UserController.java
@@ -2,9 +2,13 @@ package com.example.rest.domain.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("user")
+@RequestMapping("/user")
 public class UserController {
 	
 	private final UserServiceImpl userService;
@@ -32,6 +36,24 @@ public class UserController {
 		return ResponseEntity.ok(user);
 	}
 	
+	@PostMapping("/user")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		int result = userService.insertUser(user);
+		if(result > 0) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(user);
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+	}
 	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+		 int result = userService.deleteUser(id);
+		 if(result > 0) {
+			 return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		 }else {
+			 return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		 }
+	}
 
 }
