1f8bfc167b4fe859decfdb9c53f7dd5b1aaac799
uetak330 <uetak330@gmail.com>
2024-11-13 12:53:25 +0900

    コメントの追記
    テストメソッドにもJavadocコメントってつけるのかな？

--- src/test/java/com/example/rest/domain/repository/UserRepositoryTest.java
+++ src/test/java/com/example/rest/domain/repository/UserRepositoryTest.java
@@ -1,4 +1,4 @@
-package com.example.rest.domain;
+package com.example.rest.domain.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
@@ -13,7 +13,6 @@ import org.mockito.MockitoAnnotations;
 
 import com.example.rest.domain.mapper.UserMapper;
 import com.example.rest.domain.model.User;
-import com.example.rest.domain.repository.UserRepository;
 
 /**
  * UserRepositoryTestクラス
@@ -45,34 +44,48 @@ public class UserRepositoryTest {
 		testUser = new User(null,"uetak",28,"Osaka");
 	}
 	
+	/**
+	 * {@link UserRepository#findAll()}のテストです。
+	 * モックで設定したデータが正しく取得されるかを確認します。
+	 */
 	@Test
 	void testFindAll() {
 		when(userMapper.findAll()).thenReturn(List.of(new User(1L,"uetak", 28,"Osaka")));
 		
-		//メソッド実行
+		//メソッド実行し結果を取得
 		List<User> users = userRepository.findAll();
 		
-		//結果を検証
+		//結果のリストサイズが1であること、ユーザー名が期待通りであるか確認
 		assertNotNull(users);
 		assertEquals(1, users.size());
 		assertEquals("uetak", users.get(0).getName());
 	}
 	
+	/**
+	 * {@link UserRepository#findByIdUser(Long)}のテスト
+	 * 指定IDで取得したUserが期待通りか確認する。
+	 */
 	@Test
 	void testFindById() {
 		when(userMapper.findById(1L)).thenReturn(new User(1L,"uetak",28,"Osaka"));
 		
 		User user = userRepository.findByIdUser(1L);
 		
+		//結果がnullでない
 		assertNotNull(user);
+		//ユーザー名が期待通りであるか
 		assertEquals("uetak", user.getName());
 	}
 	
+	/**
+	 * {@link UserRepository#create(User)}のテストです。<br>
+	 * 新しいUserを作成する際の処理が期待通りか確認する。
+	 */
 	@Test
 	void testCreate() {
 		/*userMapper.insert()が呼ばれた際の動作を指定している
 		 * any(User.class)はUserオブジェクトに関するワイルドカードを意味する。
-		 * この場合、User型のに似のオブジェクトを受け入れるということ。*/
+		 * この場合、User型の疑似のオブジェクトを受け入れるということ。*/
 		when(userMapper.insert(any(User.class))).thenReturn(1);
 		
 		int result = userRepository.create(testUser);
@@ -86,16 +99,35 @@ public class UserRepositoryTest {
 		verify(userMapper,times(1)).insert(any(User.class));
 	}
 	
+	/**
+	 * {@link UserRepository#update(User)}のテストです。<br>
+	 * 既存のUser情報を更新する処理が期待通りか確認します。
+	 */
 	@Test
 	void testUpdate() {
+		/*
+		 * Mockの戻り値を設定する
+		 * userMapper.update()が呼ばれたとき、任意のlong型の値と
+		 * User型の引数を受け取った場合にint型の 1を返す
+		 */
 		when(userMapper.update(anyLong(), any(User.class))).thenReturn(1);
 		
+		//新しいUserオブジェクトを渡す
 		int result = userRepository.update(new User(1L,"kaoru", 37,"Tokyo"));
 		
 		assertEquals(1, result);
+		/*
+		 * userMapper.updateが一回呼ばれ、渡された引数が期待通りであることを検証
+		 * eq(1L)は最初の引数が1Lであることの確認、
+		 * any(User.class)は第二引数が任意のUserオブジェクトであることを確認する。
+		 */
 		verify(userMapper, times(1)).update(eq(1L),any(User.class));
 	}
 	
+	/**
+	 * {@link UserRepository#deleteById(Long)}のテストです。<br>
+	 * 指定IDのUserを削除する処理が期待通りか確認する。
+	 */
 	@Test
 	void testDeleteById() {
 		when(userMapper.deleteById(1L)).thenReturn(1);
@@ -105,6 +137,5 @@ public class UserRepositoryTest {
 		assertEquals(1,result);
 		verify(userMapper,times(1)).deleteById(1L);
 	}
-	
-	
+
 }
