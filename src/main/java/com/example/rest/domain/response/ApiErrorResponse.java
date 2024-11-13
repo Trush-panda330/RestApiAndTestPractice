fb3b89ec7492364d1f8e001092e84666526767fd
uetak330 <uetak330@gmail.com>
2024-11-13 23:06:46 +0900

    ApiErrorResponseの作成
    controllerでなにかしらエラーがあった時にこれを返そうかと思います

---
+++ src/main/java/com/example/rest/domain/response/ApiErrorResponse.java
@@ -0,0 +1,14 @@
+package com.example.rest.domain.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiErrorResponse {
+
+	private String error;
+	private String message;
+}
