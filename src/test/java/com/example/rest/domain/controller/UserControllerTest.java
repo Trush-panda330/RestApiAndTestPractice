3c9274e78543b2a4ac93d692961aab8c9c60e546
uetak330 <uetak330@gmail.com>
2024-11-13 20:35:22 +0900

    UserControllerTestの作成
    とりあえずsetUp()の定義まで

--- src/test/java/com/example/rest/domain/controller/UserControllerTest.java
+++ src/test/java/com/example/rest/domain/controller/UserControllerTest.java
@@ -1,32 +1,43 @@
 package com.example.rest.domain.controller;
-import static org.mockito.Mockito.*;
-
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.example.rest.domain.model.User;
 import com.example.rest.domain.service.UserServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class UserControllerTest {
+@WebMvcTest
+ public class UserControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Mock
+	private UserServiceImpl userServiceImpl;
+	
+	@InjectMocks
+	private UserController userController;
+	
+	private ObjectMapper objectMapper = new ObjectMapper();
+	
+	private User testUser;
+	
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+		mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+		testUser = new User(1L, "uetak", 28, "Osaka");
+	}
+	
+	
+	
 
-
-    @MockBean
-    private UserServiceImpl userService;
-
-    @Test
-    void testFindAllUsers() throws Exception {
-        // Mockデータ
-        List<User> users = List.of(new User(1L, "ueda", 28, "Osaka"));
-        when(userService.findAllUser()).thenReturn(users);
-
-
-    }
 
 }
 
